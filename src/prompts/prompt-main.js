734860648b284dd9ca09ed00efaa669bea70133c
Alace Pereira Nonato <alac_pereira@hotmail.com>
2024-11-13 21:12:05 -0300

    Import Prompt

---
+++ src/prompts/prompt-main.js
@@ -0,0 +1,10 @@
+const mainPrompt = [
+{
+    name: 'select',
+    description: 'Escolha a ferramenta (1- QrCode ou 2- PassWord)',
+    pattern: /^[1-2]+$/,
+    message: 'Escolha apenas entre 1 e 2',
+    required: true,
+}
+
+]
